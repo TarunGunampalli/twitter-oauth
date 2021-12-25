2aa992788b9b8321531bb6aac466b0f840376710
tarungunampalli <tarun.gunampalli@gmail.com>
2021-12-25 12:36:42 -0600

    custom login button

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import OAuth2Login from 'react-simple-oauth2-login';
 const onSuccess = (response) => console.log(response);
 const onFailure = (response) => console.error(response);
 
-const Login = async () => {
+const Login = () => {
 	return (
 		<OAuth2Login
 			authorizationUrl='https://twitter.com/i/oauth2/authorize'
@@ -11,6 +11,8 @@ const Login = async () => {
 			clientId={process.env.REACT_APP_CLIENT_ID}
 			redirectUri='http://localhost:3000/success'
 			scope='tweet.read'
+			isCrossOrigin
+			render={({ onClick }) => <button onClick={onClick}>Login with Twitter</button>}
 			extraParams={{ state: 'state', code_challenge: 'challenge', code_challenge_method: 'plain' }}
 			onSuccess={onSuccess}
 			onFailure={onFailure}
