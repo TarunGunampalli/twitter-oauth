35c52029fb4b200c5eefbe11ad34b076212e37f7
tarungunampalli <tarun.gunampalli@gmail.com>
2021-12-27 13:07:57 -0600

    trying to get access token

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import OAuth2Login from 'react-simple-oauth2-login';
 
 const onSuccess = (response) => console.log(response);
 const onFailure = (response) => console.error(response);
+const scopes = ['tweet.read', 'users.read'];
 
 const Login = () => {
 	return (
@@ -10,7 +11,7 @@ const Login = () => {
 			responseType='code'
 			clientId={process.env.REACT_APP_CLIENT_ID}
 			redirectUri='http://localhost:3000/success'
-			scope='tweet.read'
+			scope={scopes.join('%20')}
 			isCrossOrigin
 			render={({ onClick }) => <button onClick={onClick}>Login with Twitter</button>}
 			extraParams={{ state: 'state', code_challenge: 'challenge', code_challenge_method: 'plain' }}
